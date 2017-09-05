c3b966d735a47121bda1a64656b25c10ff4fcaf2
Sofia Kristiansen <sofiakristiansen@Sofias-MBP.lan>
2017-09-05 15:01:01 +0200

    Updated readme and removes commented code.

--- AI.php
+++ AI.php
@@ -10,28 +10,6 @@ class AI {
 
   public function sticksToRemove() {
 
-    /*switch($this->sticksLeft) {
-      case 7:
-        echo "från case 7";
-        return 2;
-        break;
-      case 4:
-      echo "från case 4";
-        return 3;
-        break;
-      case 3:
-      echo "från case 3";
-        return 2;
-        break;
-      case 2:
-      echo "från case 2";
-        return 1;
-        break;
-      default:
-      echo "från default";
-        return 1;
-    }*/
-
     if ($this->sticksLeft == 4) {
       return 3;
     } else {
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # 1dv610WS3_Sticks
+
+Behöver fixas:
+Class AI -> sticksToRemove(). De nuvarande reglerna för hur många sticks AI ska ta bort fungerar inte. Just nu hårdkodat, kanske bör slumpas fram?
+
+Class Game -> gameResult(). Returnerar ingen vinnare. Skrivs aldrig ut av index.php.
+
+Regler för hur många sticks som kan tas bort av Player. I nuvarande läge kan Player ta bort hur många sticks som helst och begränsas inte till 1-3 sticks.
+
+Utkommenterad kod i Game.php är alternativ lösning till ovanstående kod i funktionen AIRemoveSticks().
